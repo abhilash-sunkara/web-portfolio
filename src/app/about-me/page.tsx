19ccb9617b3f7869257e3ebf73e1f15b99e17f51
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-07-24 19:09:34 -0500

    Initial commit

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutMe() {
+    
+    
+    return (
+        <main className="flex min-h-screen flex-col items-start justify-start p-24 co bg-slate-950">
+        </main>
+    )
+}
