82810c62f9cdc0d1581cd5dcff91d54063542354
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2024-08-11 19:32:40 -0500

    navbar icons with about-me page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -9,6 +9,29 @@ export default function AboutMe() {
                 <h1 className = "text-slate-300 font-bold text-7xl">
                     About Me
                 </h1>
+                
+            </div>
+            <div className="w-9/12 pb-8">
+                <h1 className="text-2xl text-slate-300 text-justify">
+                    I am a current senior in highschool and I'm an aspiring engineer and software 
+                    developer. I'm planning to pursue an electrical computer engineering major while 
+                    refining my own skills in web development, designing and manufacturing robots and 
+                    creating performant applications.
+                </h1>
+            </div>
+            <div className="w-9/12 pb-8">
+                <h1 className="text-2xl text-slate-300 text-justify">
+                    I have experience with CAD programs and the engineering design process through 
+                    leading my FTC Robotics team, 21438 Chaotic Current, for 2 years. During this time,
+                     we've made it to the Texas States competition in 2022 and finalized for the Inspire award 
+                     in 2023. I also refined my skills with data collection, sensor usage and OOP through my work here.
+                </h1>
+            </div>
+            <div className="w-9/12 pb-8">
+                <h1 className="text-2xl text-slate-300 text-justify">
+                    My future aspirations include learning more about Rust and low level programming 
+                    while also improving my web-dev skills with WASM, React and other technologies
+                </h1>
             </div>
             <NavBar></NavBar>
         </main>
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { usePathname, useRouter } from "next/navigation";
-
+import { FaLightbulb, FaRobot, FaCode } from "react-icons/fa";
+import { FaFaceGrin } from "react-icons/fa6";
 
 const NavBar = () => {
   const router = useRouter()
@@ -18,25 +19,29 @@ const NavBar = () => {
 
   return (
     <div className="fixed top-0 right-0 h-screen w-32 flex flex-col justify-evenly bg-slate-300 text-slate-950">
-      <div className = {`navbar-default ${lastSegment === 'skills' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'}`} onClick={() => handleClick("skills")}>
-        <h1 className="text-xl">
+      <div className = {`navbar-default ${lastSegment === 'skills' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'} group`} onClick={() => handleClick("skills")}>
+        <h1 className="text-xl hidden group-hover:inline-block">
           Skills
-        </h1>
+        </h1> 
+        <FaLightbulb size = {36} className="inline-block group-hover:hidden"></FaLightbulb> 
       </div>
-      <div className = {`navbar-default ${lastSegment === 'robotics-projects' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'}`} onClick={() => {handleClick("robotics-projects")}}>
-        <h1 className="text-xl text-center">
+      <div className = {`navbar-default ${lastSegment === 'robotics-projects' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'} group`} onClick={() => {handleClick("robotics-projects")}}>
+        <h1 className="text-xl text-center hidden group-hover:inline-block">
           Robotics Projects
         </h1>
+        <FaRobot size = {36} className="inline-block group-hover:hidden"></FaRobot>
       </div>
-      <div className = {`navbar-default ${lastSegment === 'software-projects' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'}`} onClick={() => {handleClick("software-projects")}}>
-        <h1 className="text-xl text-center">
+      <div className = {`navbar-default ${lastSegment === 'software-projects' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'} group`} onClick={() => {handleClick("software-projects")}}>
+        <h1 className="text-xl text-center hidden group-hover:inline-block">
           Software Projects
         </h1>
+        <FaCode size = {36} className="inline-block group-hover:hidden"></FaCode>
       </div>
-      <div className = {`navbar-default ${lastSegment === 'about-me' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'}`} onClick={() => {handleClick("about-me")}}>
-        <h1 className="text-xl">
+      <div className = {`navbar-default ${lastSegment === 'about-me' ? 'navbar-selected hover:navbar-hovered' : 'hover:navbar-hovered'} group`} onClick={() => {handleClick("about-me")}}>
+        <h1 className="text-xl text-center hidden group-hover:inline-block">
           About Me
         </h1>
+        <FaFaceGrin size = {36} className="inline-block group-hover:hidden"/>
       </div>
     </div>
   );
